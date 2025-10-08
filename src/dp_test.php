a6a4e348850b62892c009d7384c6efacd51e2454
petru-robu <petru.r.robu@gmail.com>
2025-10-08 10:38:10 +0300

    added docker

---
+++ src/dp_test.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli = new mysqli("db", "appuser", "apppass", "appdb");
+
+if ($mysqli->connect_error) 
+{
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+echo "Connected successfully";
+$mysqli->close();
