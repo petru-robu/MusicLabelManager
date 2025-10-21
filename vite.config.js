0b1cd225e13f09598c7da6dfdab75e1f38f2eb35
petru-robu <petru.r.robu@gmail.com>
2025-10-21 16:11:34 +0300

    vite server fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
     server: {
