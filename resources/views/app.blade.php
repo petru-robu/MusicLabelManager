3121dd8574621c7126f84268797d3c83fe5aa57a
petru-robu <petru.r.robu@gmail.com>
2025-10-21 17:12:32 +0300

    added navbar as layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
