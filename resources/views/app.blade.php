f926a88567e207f768056db40e4af36e399bfae3
petru-robu <petru.r.robu@gmail.com>
2025-10-22 18:20:19 +0300

    fix prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
 
-    @viteReactRefresh
+    
     @vite('resources/js/app.jsx')
     @inertiaHead
   </head>
